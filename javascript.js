28aff301b1746c04af834c32c7d941fb5b036ebe
anishmdhr <anishmdhr01@gmail.com>
2022-12-16 19:07:05 +0545

    Add comments to JS file

--- javascript.js
+++ javascript.js
@@ -1,5 +1,19 @@
 function randomInteger(min, max){
     return Math.floor(Math.random() * (max-min +1)) + min;
+/*
+Useful examples(integer):
+// 0 -> 10
+Math.floor(Math.random() * 11);
+
+// 1 -> 10
+Math.floor(Math.random() * 10) + 1;
+
+// 5 -> 20
+Math.floor(Math.random() * 16) + 5;
+
+// -10 -> (-2)
+Math.floor(Math.random() * 9) - 10;
+*/
 }
 
 function getComputerChoice(){
